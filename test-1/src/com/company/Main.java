c4e583b533df64385a25bd4d62be26d95c21301c
wojtas5127 <73577427+wojtas5127@users.noreply.github.com>
2021-11-10 16:24:50 +0100

    Add files via upload

---
+++ test-1/src/com/company/Main.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Random;
+import java.lang.StringBuffer;
+
+public class Main {
+
+
+//    Zad 1
+//
+//    Wczytać liczbę naturalną n, a następnie wczytując kolejno n liczb naturalnych,
+//    napisz publiczną statyczną metodę showPrimes(int[], list), która zwróci tablicę
+//    pokazującą pozycje składające się z liczb pierwszych w podanej tablicy `list’.
+
+//    public static int showPrimes(int[] list) {
+//
+//        int tab[]=list;
+//
+//        for (int i = 0; i < list.length; i++) {
+//
+//            if(tab[i]==2 || tab[i]==3){
+//            }
+//
+//            if(tab[i]%2!=0 & tab[i]%3!=0){
+//            }
+//        }
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        int n;
+//        System.out.print("Podaj n: ");
+//        n = scan.nextInt();
+//        int tab[] = new int[n];
+//
+//        for (int i = 0;i < n;i++){
+//        }
+//    }
+
+//  Zad 2
+//
+//    Napisz program (za pomocą funkcji), który wczyta do zmiennej n liczbę naturalną,
+//    a potem wygeneruje n wiodących elementów tablicy o wartościach całkowitych od -
+//            50 do 50, a następnie wyszuka najczęstszy element występujący w tablicy i wyliczy
+//    liczbę razy pojawia się w tablicy.
+
+
+//    public static void main(String[] args) {
+//
+//        Scanner scan = new Scanner(System.in);
+//        int n;
+//        System.out.print("Podaj n: ");
+//        n = scan.nextInt();
+//        Random r = new Random();
+//        int tab[] = new int[n];
+//
+//        for (int i = 0; i < n;i++){
+//
+//            tab[i] = r.nextInt((50+50)-50);
+//            System.out.print(tab[i]+"\n");
+//        }
+//
+//    }
+
+//  Zad 3
+
+//    Napisz publiczną statyczną metodę modify(String str), która znajduje środek ciągu,
+//    jeśli ciąg ma parzystą długość, i zmienia go z małych liter na wielkie. Następnie w
+//    końcu zwróć zmodyfikowany ciąg. W przeciwnym razie zwraca oryginalny ciąg str.
+//    Na przykład modify(middle) zwraca miDDle, a modify(mad) zwraca mad.
+
+
+}
